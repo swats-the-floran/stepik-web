2bfb1739569e08096153ee432084a62754040d95
mfaust <euc.anatoly@gmail.com>
2018-11-18 03:38:01 +0500

    django

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
+sudo apt-get update
+sudo apt-get install python3.5
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python3.5 get-pip.py
