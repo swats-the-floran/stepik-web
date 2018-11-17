37ce5a4d446ec12001f8f3502854b998a80ebf4f
mfaust <euc.anatoly@gmail.com>
2018-11-18 03:01:47 +0500

    django

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
