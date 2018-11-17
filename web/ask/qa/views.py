e617458d6596d2fb1c2c8aa45b99fbc2e689e62f
mfaust <euc.anatoly@gmail.com>
2018-11-18 01:52:06 +0500

    django

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+
+def root(request, *args, **kwargs):
+    return HttpResponse(status=404)
+
+
+def login(request, *args, **kwargs):
+    return HttpResponse(status=404)
+
+
+def signup(request, *args, **kwargs):
+    return HttpResponse(status=404)
+
+
+def question(request, id, *args, **kwargs):
+    print(id)
+    print(type(id))
+
+    return HttpResponse(id)
+
+
+def ask(request, *args, **kwargs):
+    return HttpResponse(status=404)
+
+
+def popular(request, *args, **kwargs):
+    return HttpResponse(status=404)
+
+
+def new(request, *args, **kwargs):
+    return HttpResponse(status=404)
