204b422ba658839ae64d214cfc34aa56804bf1c2
mfaust <euc.anatoly@gmail.com>
2018-11-23 19:44:36 +0500

    fuck mysql

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -15,9 +15,9 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.IntegerField()
+    likes = models.ManyToManyField(User, related_name='q_to_likes')
 
 
 class Answer(models.Model):
