b3285c7d0d86dc5f799d8529c3d14ea624adb429
mfaust <euc.anatoly@gmail.com>
2018-11-23 05:08:29 +0500

    django

--- init.sh
+++ init.sh
@@ -1,31 +1,36 @@
-﻿# don't forget that in gunicorn configs python 3.5 should be used
+﻿# dont forget that in gunicorn configs python 3.5 should be used
 # all changed filed i added to an "edinorogs" directory
-sudo apt-get update
-sudo apt-get -f install libffi-dev libssl-dev
+apt-get update
+#apt-get -y install libffi-dev
 # for python 3.5
-#apt-get install -f python3.5
-#curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-#python3.5 get-pip.py
-#installing openssl of acceptable version
-git clone https://github.com/swats-the-floran/stepik-python37
-cd stepik-python37/openssl-1.1.0j/
-sudo make install
-sudo ldconfig
-cd ../../
+apt-get install -f python3.5
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python3.5 get-pip.py
+# installing openssl of acceptable version
+#git clone https://github.com/swats-the-floran/stepik-python37
+#cd stepik-python37/openssl-1.1.0j/
+#sudo make install
+#sudo ldconfig
+#cd ../../
 # installing python3.7.1
-cd stepik-python37/Python-3.7.1/
-sudo -H make install
-cd ../../
+#cd stepik-python37/Python-3.7.1/
+#sudo -H make install
+#cd ../../
 # installing libraries
-sudo -H pip3.7 install django
-sudo -H pip3.7 install gunicorn
+pip install django
+pip install gunicorn
 # configs
-sudo rm /etc/nginx/sites-enabled/default
-sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo cp /home/box/web/etc/guni*   /etc/gunicorn.d/
-sudo cp edinorogs/gunicorn-debian /usr/sbin/
-sudo cp edinorogs/gunicorn /usr/bin/
-sudo cp edinorogs/gunicorn_* /usr/sbin/
+rm /etc/nginx/sites-enabled/default
+cp web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+cp web/etc/guni*   /etc/gunicorn.d/
+cp edinorogs/gunicorn-debian /usr/sbin/
+cp edinorogs/gunicorn /usr/bin/
+cp edinorogs/gunicorn_* /usr/sbin/
 # launching and relaunching services
+sudo /etc/init.d/mysql start
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restartc
+# other stuff
+mysql -u root -e "CREATE DATABASE stepik CHARACTER SET utf8;"
+./manage.py makemigrations
+./manage.py migrate
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        pass
+
+    def popular(self):
+        pass
+
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = User
+    likes = models.IntegerField()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = User
