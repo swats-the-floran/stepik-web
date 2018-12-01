6747d9ce30871db90708a7c3dddd47cbd6c10618
mfaust <euc.anatoly@gmail.com>
2018-12-01 08:20:55 +0500

    added paginator code and templates

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.root, name='root'),
+    # path('', views.root, name='root'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('question/<int:pk>/', views.question, name='question'),
     path('ask/', views.ask, name='ask'),
     path('popular/', views.popular, name='popular'),
-    path('new/', views.new, name='new'),
+    path('', views.new, name='new'),
 ]
